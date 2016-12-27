0685ac274227d66a61feef715e7c21ab1ba1f525
fc.w <fcwang@86links.com>
2016-12-27 12:10:03 +0800

    del and modified file

--- smart-scala/src/main/scala/Test2.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by  fc.w on 2016/12/27.
- */
-public class Test2 {
-}
--- smart-scala/src/main/scala/com/dodoca/smart/Test1.java
+++
@@ -1,7 +0,0 @@
-package com.dodoca.smart;
-
-/**
- * Created by  fc.w on 2016/12/27.
- */
-public class Test1 {
-}
