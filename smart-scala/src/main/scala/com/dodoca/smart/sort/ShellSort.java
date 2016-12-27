85418b7fe000e2cebaf3c807652239cb81ecaa1d
fc.w <fcwang@86links.com>
2016-12-27 19:52:18 +0800

    add sort algorithm

--- smart-scala/src/main/scala/com/dodoca/smart/sort/ShellSort.java
+++ smart-scala/src/main/scala/com/dodoca/smart/sort/ShellSort.java
@@ -8,21 +8,27 @@ public class ShellSort {
 
     public static void main(String[] args) {
         int[] eles = {2, 35, 23, 10, 7, 9, 30, 50, 66};
-        int h = eles.length / 2;
-        while (h > 1) {
-            for (int i = h; i < eles.length; i++) {
-                int j = i - h;
-                int tmp = eles[i];
-                for (; j >= 0 && eles[j] > tmp; j = j - h) {
-                    eles[j + h] = eles[j];
+        int a = eles.length;
+        int temp = 0;
+        while (true) {
+            a = a / 2;
+            for (int x = 0; x < a; x++) {
+                for (int i = x + a; i < eles.length; i += a) {
+                    int j = i - a;
+                    temp = eles[i];
+                    for (; j >= 0 && temp <eles[j]; j -= a) {
+                        eles[j + a] = eles[j];
+                    }
+                    eles[j + a] = temp;
                 }
-                eles[j + h] = tmp;
             }
-            h = h / 2;
+
+            if (a == 1) break;
         }
 
-        for (int ele : eles) {
-            System.out.println(ele);
+        for (int i : eles) {
+            System.out.print(i +"\t");
         }
+
     }
 }
